d402da162e09c625e277c1ca1b958e72c1a4f04a
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2025-03-29 02:42:50 +0530

    hero section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
+import Hero from "@/components/hero";
+import HomeProject from "@/components/homeproject";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-b from-black to-slate-900 flex flex-col min-h-screen">
+    // <div className="bg-gradient-to-b from-black to-slate-950 flex flex-col min-h-screen">
+    <div className="bg-zinc-900 flex flex-col min-h-screen">
       <Navbar />
 
-      {/* <section className="container border border-gray-200">
-      <div className="">hey</div>
-    </section> */}
+      <section className="container border border-gray-200">
+        <Hero />
+      </section>
+
+      <section className="container border border-gray-200">
+        <HomeProject />
+      </section>
     </div>
   );
 }
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="flex justify-between items-start pt-20">
+      <div className="">
+        <h1 className="text-5xl font-sans font-bold text-white mb-2">Myself, Kuntal</h1>
+        <p className="text-md font-sans text-zinc-400">
+          As a <span className="text-cyan-400 font-bold bg-zinc-700 px-2 py-1">Full Stack</span> Engineer building websites.
+        </p>
+        <br />
+        <p className="text-md font-sans text-zinc-400 mb-2">Exploring new technologies and creating web experiences, while continuously learning and growing as a developer.</p>
+        <hr className="border-zinc-700 w-1/2 "/>
+        <p className="text-md font-sans text-zinc-400 mt-2">Find me on <span className="text-cyan-400 font-semibold cursor-pointer">twitter</span> for tech updates and memes.</p>
+      </div>
+      <div className="relative h-20 w-20 rounded-sm">
+        <div className="absolute -bottom-2 -right-2 h-full w-full border-2 border-cyan-400"></div>
+        <div className="absolute -top-2 -left-2 h-full w-full border-2 border-cyan-400"></div>
+        <Image 
+          src="/me.jpeg" 
+          alt="my-image" 
+          fill 
+          className="rounded-sm relative z-10 object-cover"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
