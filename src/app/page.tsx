a165e1e457dc338cc38c1091b31a88bac34e70b9
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2025-03-28 23:12:02 +0530

    navbar added

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-gradient-to-b from-black to-slate-900 flex flex-col min-h-screen">
+      <Navbar />
+
+      {/* <section className="container border border-gray-200">
+      <div className="">hey</div>
+    </section> */}
+    </div>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import { Sun } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+
+const Navbar = () => {
+  return (
+    <div className="py-8 px-[35rem] w-full text-md font-semibold text-white flex justify-between items-center border border-gray-600">
+      <div>logo</div>
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-6 border border-stone-700 rounded-4xl px-6 py-1">
+          <Link href="/" className="text-cyan-400 cursor-pointer font-normal">Home</Link>
+          <Link href="/about" className="cursor-pointer">About</Link>
+          <Link href="/project" className="cursor-pointer">Project</Link>
+          <Link href="/contact" className="cursor-pointer">Contact</Link>
+        </div>
+        <div className="align-center text-2xl flex items-center justify-center border border-cyan-400 rounded-2xl w-10 h-8">
+          <Sun size={15} className="text-cyan-400"/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
