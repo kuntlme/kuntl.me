4d84e07094901e5950b3437ccafdf2228895a8d3
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2025-03-30 12:34:42 +0530

    some style changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,21 @@
+"use client"
 import { Sun } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 
 const Navbar = () => {
+  const pathname = usePathname();
   return (
     <div className="fixed py-8 px-[35rem] w-full bg-zinc-900 text-md font-semibold text-white flex justify-between items-center z-50">
       <div>logo</div>
       <div className="flex items-center gap-4">
-        <div className="flex items-center gap-6 border border-stone-700 rounded-4xl px-6 py-1">
-          <Link href="/" className="text-cyan-400 cursor-pointer font-normal">Home</Link>
-          <Link href="/about" className="cursor-pointer">About</Link>
-          <Link href="/project" className="cursor-pointer">Project</Link>
-          <Link href="/contact" className="cursor-pointer">Contact</Link>
+        <div className="flex items-center gap-6 border border-stone-700 rounded-4xl px-6">
+          <Link href="/" className={`${pathname == '/' ? "text-cyan-400 font-normal border-b border-cyan-600" : "text-white font-semibold"} cursor-pointer py-1`}>Home</Link>
+          <Link href="/about" className={`${pathname == '/about'? "text-cyan-400 font-normal border-b border-cyan-600" : "text-white font-semibold"} cursor-pointer py-1`}>About</Link>
+          <Link href="/project" className={`${pathname == '/project'? "text-cyan-400 font-normal border-b border-cyan-600" : "text-white font-semibold"} cursor-pointer py-1`}>Project</Link>
+          <Link href="/contact" className={`${pathname == '/contact'? "text-cyan-400 font-normal border-b border-cyan-600" : "text-white font-semibold"} cursor-pointer py-1`}>Contact</Link>
         </div>
         <div className="align-center text-2xl flex items-center justify-center border border-cyan-400 rounded-2xl w-10 h-8">
           <Sun size={15} className="text-cyan-400"/>
