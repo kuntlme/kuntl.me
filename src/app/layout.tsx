2cecb1781d4e87483b89dc88ef65a2caaf80e88a
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2025-05-16 16:31:56 +0530

    edited layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,10 +28,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap"
-        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
